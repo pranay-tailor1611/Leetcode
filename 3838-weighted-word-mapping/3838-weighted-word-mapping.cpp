a53afed99a723c45ebb08ee9fd14ce7f69b95baf
Pranay Tailor <pranaytailor1611@gmail.com>
2026-08-10 16:00:38 +0530

    Time: 4 ms (17.03%), Space: 43.7 MB (57.78%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans = "";
+
+        for(string word : words){
+
+            int sum = 0;
+
+            for(char ch : word){
+
+                sum += weights[ch - 'a'];
+            }
+            int rem = sum % 26;
+            char map = 'z' - rem;
+            ans += map;
+        }
+        return ans;
+    }
+};
